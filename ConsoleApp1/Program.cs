96f0f3dc125f259912ddee7b5a018c31a3d721bb
awolferret <104518827+awolferret@users.noreply.github.com>
2022-04-28 21:36:54 +0300

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сколько у вас золота?");
+            int gold = Convert.ToInt32(Console.ReadLine());
+            int cristalPrice = 14;
+            int possibleToBuy = gold / cristalPrice;
+            Console.WriteLine($"Вы можете купить {possibleToBuy} кристаллов");
+            Console.WriteLine("Сколько вы хотите купить?");
+            int buy = Convert.ToInt32(Console.ReadLine());
+            int goldRemains = gold - (cristalPrice * buy);
+            Console.WriteLine($"Вы купили {buy} кристаллов и у вас осталось {goldRemains}");
+        }
+    }
+}
